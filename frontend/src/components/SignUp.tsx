db49f757ec23a7d504e14036b83fe883cfc95adb
karlodinikozic <58643902+karlodinikozic@users.noreply.github.com>
2021-04-15 15:59:52 +0200

    Initi Singup componente

---
+++ frontend/src/components/SignUp.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const SignUp : React.FC = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SignUp
+ 

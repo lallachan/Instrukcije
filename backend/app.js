391f072a276f43722eafa10681304be4fbba9a81
karlodinikozic <58643902+karlodinikozic@users.noreply.github.com>
2021-04-16 02:13:55 +0200

    removed error

--- backend/app.js
+++ backend/app.js
@@ -28,14 +28,18 @@ const ladingRoute = require('./routes/landing')
 
 
 // router Middlewares
-app.use(express.urlencoded({ extended: true })); maybe someday
+app.use(express.urlencoded({ extended: true })); //maybe someday
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(cors())
+
+
 app.use('/api/user',authRoute)
 app.use('/api/landing',ladingRoute)
 
-
+app.get("/",(req,res)=>{
+    res.send("UP")
+})
 
 app.listen(process.env.PORT || 5000, async ()=>{
     const instruktors = await Instruktor_Landing.find({})

2db93710e9f745c60323ac7bf8d3557fab0650d0
lallachan <30838056+lallachan@users.noreply.github.com>
2021-04-18 22:50:08 +0200

    register i login i vucenje cardsa

--- frontend/src/components/SignUpForm.tsx
+++ frontend/src/components/SignUpForm.tsx
@@ -8,48 +8,113 @@ import {
   Icon,
   Button,
 } from "@chakra-ui/react";
+import axios, { AxiosResponse } from "axios";
 
 import React from "react";
-import {FaEnvelope,FaInfoCircle,FaLock} from 'react-icons/fa'
+import { useForm } from "react-hook-form";
+import { FaEnvelope, FaInfoCircle, FaLock } from "react-icons/fa";
+
+type RegisterUser = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
 
 interface Props {}
 
 export const SignUpForm: React.FC = (props: Props) => {
+  const { register, handleSubmit } = useForm<RegisterUser>();
+
+  const onSubmit = handleSubmit( async(data) => {
+
+    try {
+    
+    console.log(data)
+    const response = await axios.post(process.env.REACT_APP_SERVER_CONNECT + "/api/user/register", data)
+    console.log(response)
+
+    } catch (error) {
+      console.log(error.response.data)
+    }
+
+  }
+  
+  );
+
+
+
   return (
-    <form action="submit">
-      <Stack spacing={4} >
-        <FormControl isRequired >
-        <InputGroup >
+    <form onSubmit={onSubmit}>
+      {/* TODO ADD VALIDATION */}
+      <Stack spacing={4}>
+        <FormControl isRequired>
+          <InputGroup>
             <InputLeftElement children={<FaInfoCircle />} />
-            <Input bg="white" _hover={{border:"2px solid teal"}} type="name" placeholder="First name" aria-label="First name" />
+            <Input
+              {...register("firstName")}
+              bg="white"
+              _hover={{ border: "2px solid teal" }}
+              type="name"
+              placeholder="First name"
+              aria-label="First name"
+            />
           </InputGroup>
         </FormControl>
         <FormControl isRequired>
-        <InputGroup>
+          <InputGroup>
             <InputLeftElement children={<FaInfoCircle />} />
-            <Input bg="white" _hover={{border:"2px solid teal"}} type="name" placeholder="Last name" aria-label="Last name" />
+            <Input
+              {...register("lastName")}
+              bg="white"
+              _hover={{ border: "2px solid teal" }}
+              type="name"
+              placeholder="Last name"
+              aria-label="Last name"
+            />
           </InputGroup>
         </FormControl>
-        <Divider borderColor='gray.300'/>
+        <Divider borderColor="gray.300" />
 
         <FormControl isRequired>
           <InputGroup>
             <InputLeftElement children={<FaEnvelope />} />
-            <Input  bg="white" _hover={{border:"2px solid teal"}} type="email" placeholder="Email" aria-label="Email" />
+            <Input
+              {...register("email")}
+              bg="white"
+              _hover={{ border: "2px solid teal" }}
+              type="email"
+              placeholder="Email"
+              aria-label="Email"
+            />
           </InputGroup>
         </FormControl>
 
         <FormControl isRequired>
           <InputGroup>
             <InputLeftElement children={<FaLock />} />
-            <Input bg="white" _hover={{border:"2px solid teal"}} type="password" placeholder="Password" aria-label="Password" />
+            <Input
+              {...register("password")}
+              bg="white"
+              _hover={{ border: "2px solid teal" }}
+              type="password"
+              placeholder="Password"
+              aria-label="Password"
+            />
           </InputGroup>
         </FormControl>
 
-        <Button type="submit"  backgroundColor="teal" color="white" variant="solid" _hover={{boxShadow:'md'}} _active={{boxShadow:'lg'}} >
+        <Button
+          type="submit"
+          backgroundColor="teal"
+          color="white"
+          variant="solid"
+          _hover={{ boxShadow: "md" }}
+          _active={{ boxShadow: "lg" }}
+        >
           Sign up!
         </Button>
-        <br/>
+        <br />
       </Stack>
     </form>
   );
